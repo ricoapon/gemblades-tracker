0d6c58f442ad02670c79e8114df835b36ddbd18d
Rico Apon <rico.apon@gmail.com>
2026-08-08 20:29:04 +0000

    Log resources

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
@@ -61,6 +63,7 @@ static class HorizontalCardHolderBanishSpecificCardPatch
 }
 
 // Covers the "evolve" mechanic.
+// TODO: this is actually wrong. It triggers on removing all cards from the store.
 [HarmonyPatch(typeof(Card), "PolymorphToPersona")]
 public static class CardPolymorphToPersonaPatch
 {
@@ -70,5 +73,38 @@ public static class CardPolymorphToPersonaPatch
         string newName = templatePersona?.personaName ?? "<none>";
 
         Plugin.Log.LogInfo($"Card evolved: '{oldName}' -> '{newName}'");
+    }
+}
+
+// Covers all the increase/decrease methods that are relevant. However, SpendResources also updates these values.
+// I decided to only cover the actual values themselves. Seems easier to store than a list of delta's, which the
+// tool reading this can also determine for itself. And less prone to bugs in case we miss a method.
+// Voters = gems. Fame = no clue?
+[HarmonyPatch]
+static class PlayerManagerResourcePatch
+{
+    static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.IncreaseMoney));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.DecreaseMoney));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.IncreasePower));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.DecreasePower));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.IncreaseFame));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.DecreaseFame));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.IncreaseVoters));
+        yield return AccessTools.Method(typeof(PlayerManager), nameof(PlayerManager.DecreaseVoters));
+    }
+
+    static void Postfix(PlayerManager __instance, MethodBase __originalMethod, int amount)
+    {
+        Plugin.Log.LogInfo($"Money: {__instance.money}, Power: {__instance.power}, Fame: {__instance.fame}, Voters: {__instance.voters}");
+    }
+}
+[HarmonyPatch(typeof(PlayerManager), "SpendResources")]
+public static class PlayerManagerSpendResourcesPatch
+{
+    static void Prefix(PlayerManager __instance, int moneyCost, int fameCost, int powerCost, int votersCost)
+    {
+        Plugin.Log.LogInfo($"Money: {__instance.money}, Power: {__instance.power}, Fame: {__instance.fame}, Voters: {__instance.voters}");
     }
 }
