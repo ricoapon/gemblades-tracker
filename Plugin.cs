eeaac708f98f633f2cb5c226c67f7861f66fccc9
Rico Apon <rico.apon@gmail.com>
2026-08-08 21:13:34 +0000

    Simplify

--- Plugin.cs
+++ Plugin.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 
 namespace GembladesTracker;
 
+// Important note: we only keep track of resources and not the individual cards.
+// The code is a bit difficult to patch in all places where any kind of change is made.
+// Purchase is doable, but evolving or banishing with different abilities is prone
+// to have some bugs. So we forgo this and just record the total deck size.
 [BepInPlugin("com.ricoapon.gemblades-tracker", "Gemblades Tracker", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
@@ -21,59 +25,24 @@ public class Plugin : BaseUnityPlugin
     }
 }
 
-[HarmonyPatch(typeof(ShopCardHolder), "ContinuePurchase")]
-static class ShopCardHolderContinuePurchasePatch
-{
-    static void Postfix(Card card, Persona persona)
-    {
-        // Boss has a separate boolean, but it is also considered an enemy.
-        Plugin.Log.LogInfo(
-            $"{(persona.isEnemy ? "Purchased Enemy" : "Purchased Card")}: {persona.personaName}"
-        );
-    }
-}
-
 // For some reason, StartTurn only triggers from turn 2 and afterwards. This method is triggered when you open the
 // main menu. This is good enough I guess. When you exit to main menu, it shows this. Could function as a
 // "the game stopped" message as well.
 [HarmonyPatch(typeof(PlayerManager), "Start")]
 static class PlayerManagerStartPatch
 {
-    static void Postfix(ref int ___currentTurn)
+    static void Postfix(PlayerManager __instance, ref int ___currentTurn)
     {
-        Plugin.Log.LogInfo($"Turn started: {___currentTurn}");
+        Plugin.Log.LogInfo($"Turn started: {___currentTurn} with deck size {__instance.cardHolder.GetTotalDeckCount()}");
     }
 }
 
 [HarmonyPatch(typeof(PlayerManager), "StartTurn")]
 static class PlayerManagerStartTurnPatch
 {
-    static void Postfix(ref int ___currentTurn)
+    static void Postfix(PlayerManager __instance, ref int ___currentTurn)
     {
-        Plugin.Log.LogInfo($"Start turn {___currentTurn}");
-    }
-}
-
-[HarmonyPatch(typeof(HorizontalCardHolder), "BanishSpecificCard")]
-static class HorizontalCardHolderBanishSpecificCardPatch
-{
-    static void Postfix(Card card)
-    {
-        Plugin.Log.LogInfo( $"Removed from deck: {card.persona.personaName}");
-    }
-}
-
-// Covers the "evolve" mechanic.
-// TODO: this is actually wrong. It triggers on removing all cards from the store.
-[HarmonyPatch(typeof(Card), "PolymorphToPersona")]
-public static class CardPolymorphToPersonaPatch
-{
-    static void Prefix(Card __instance, Persona templatePersona)
-    {
-        string oldName = __instance?.persona?.personaName ?? "<none>";
-        string newName = templatePersona?.personaName ?? "<none>";
-
-        Plugin.Log.LogInfo($"Card evolved: '{oldName}' -> '{newName}'");
+        Plugin.Log.LogInfo($"Turn started: {___currentTurn} with deck size {__instance.cardHolder.GetTotalDeckCount()}");
     }
 }
 
