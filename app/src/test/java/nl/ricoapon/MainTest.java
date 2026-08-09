1110459fd4e097ad999d411863612c9c7fc99ec1
Rico Apon <rico.apon@gmail.com>
2026-08-09 10:08:48 +0200

    Initialize Gradle project

---
+++ app/src/test/java/nl/ricoapon/MainTest.java
@@ -0,0 +1,12 @@
+package nl.ricoapon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void works() {
+        assertTrue(true);
+    }
+}
